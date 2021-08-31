4d44525ebf8f5ff191b1a7f8ee19ca4a13ac2ea7
Chibuzor Ihueze <krisykord@gmail.com>
2021-08-31 04:33:30 -0700

    put char

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-
-int main()
+#include "holberton.h"
+/**
+*main - prints Holberton
+*Return: 0
+*/
+int main(void)
 {
-  if(printf("Holberton")){
-    
-  }
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
